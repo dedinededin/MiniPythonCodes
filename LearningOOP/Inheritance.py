d103364e4ed71234075515443ced69c02a45f476
dedinededin <41920662+dedinededin@users.noreply.github.com>
2018-12-03 02:02:33 +0300

    Inheritance and super() is understood

---
+++ LearningOOP/Inheritance.py
@@ -0,0 +1,43 @@
+class UniversityPeople:
+
+    def __init__(self, fname, lname, email, id):
+        self.fname = fname
+        self.lname = lname
+        self.email = email
+        self.id = id
+
+    def fullname(self):
+        print(self.fname + " " + self.lname)
+
+    def __str__(self):
+        return self.fname + " " + self.lname + " " + self.email + " " + self.id
+
+
+class Students(UniversityPeople):
+    pass
+
+
+class Teachers(UniversityPeople):
+    def __init__(self, fname, lname, email, id, salary):
+        super().__init__(fname, lname, email, id)
+        self.salary = salary
+
+
+# nested inheritance Maths inherits both Teachers and UniversityPeople classes.
+class Maths(Teachers):
+    pass
+
+
+stu1 = Students("Abraham", "Dogan", "abraham@dogan.com", "1")
+stu2 = Students("Ismet", "Deniz", "ismet@deniz.com", "2")
+maths = Teachers("Emma", "Watson", "emma@watson.com", "3", "97000")
+
+stu1.fullname()
+maths.fullname()
+print(maths.salary)
+print(maths)
+
+print(isinstance(stu1, Students))
+print(isinstance(stu1, Teachers))
+print(isinstance(stu1, UniversityPeople))
+print(issubclass(Students, UniversityPeople))
